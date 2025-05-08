f275aae3ad4b5980a64b85f7804ac692eabb5d51
midleg <lorisbaj2010@gmail.com>
2025-05-08 03:55:16 -0700

    Update WhoPingedMe.tsx

--- WhoPingedMe/WhoPingedMe.tsx
+++ WhoPingedMe/WhoPingedMe.tsx
@@ -1,49 +1,48 @@
-// WhoPingedMe.tsx
-import { Plugin } from "vencord";
+// src/plugins/WhoPingedMe.tsx
+
+import { Plugin } from "@modules/plugin";
 import { findByProps } from "@webpack";
+import { getCurrentUser } from "@utils/user";
 import { getGuildId } from "@utils/discord";
-import { addListener, removeListener } from "@utils/events";
 
 const MessageStore = findByProps("getMessages");
 const UserStore = findByProps("getUser");
 
 export default class WhoPingedMe implements Plugin {
-    onStart() {
-        window.addEventListener("keydown", this.keyHandler);
-    }
-
-    onStop() {
-        window.removeEventListener("keydown", this.keyHandler);
-    }
-
     keyHandler = (e: KeyboardEvent) => {
         if (e.ctrlKey && e.altKey) {
+            const userId = getCurrentUser()?.id;
             const guildId = getGuildId();
-            if (!guildId) return alert("You must be in a server!");
 
-            const channelMessages = MessageStore.getMessages();
-            const mentions: string[] = [];
+            if (!userId || !guildId) {
+                alert("Please open a server first.");
+                return;
+            }
 
-            for (const [channelId, messageStore] of channelMessages.entries()) {
-                for (const msg of messageStore._array) {
-                    if (msg.mentions?.includes(UserStore.getCurrentUser().id)) {
+            const allMessages = MessageStore.getMessages();
+            const mentioners: Set<string> = new Set();
+
+            for (const channelId in allMessages) {
+                const msgs = allMessages[channelId]?._array ?? [];
+                for (const msg of msgs) {
+                    if (msg?.mentions?.includes(userId)) {
                         const user = UserStore.getUser(msg.author.id);
-                        if (user && !mentions.includes(user.username)) {
-                            mentions.push(user.username);
-                        }
+                        if (user) mentioners.add(user.username);
                     }
                 }
             }
 
             const html = `
                 <html>
-                    <head><title>Who Pinged Me</title></head>
+                    <head><title>Mentions</title></head>
                     <body>
-                        <h1>Pings in this server:</h1>
+                        <h1>People who pinged you in this server:</h1>
                         <ul>
-                            ${mentions.length > 0
-                                ? mentions.map(m => `<li>${m}</li>`).join("")
-                                : "<li>No mentions found</li>"}
+                            ${
+                                mentioners.size
+                                    ? Array.from(mentioners).map(m => `<li>${m}</li>`).join("")
+                                    : "<li>No mentions found</li>"
+                            }
                         </ul>
                     </body>
                 </html>
@@ -54,4 +53,12 @@ export default class WhoPingedMe implements Plugin {
             window.open(url, "_blank");
         }
     };
+
+    onStart() {
+        window.addEventListener("keydown", this.keyHandler);
+    }
+
+    onStop() {
+        window.removeEventListener("keydown", this.keyHandler);
+    }
 }
